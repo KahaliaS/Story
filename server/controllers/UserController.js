0fe13c65f4672f76c45031fa7cce29e056f677d8
Kahalia Stanberry <kahalia.stanberry@gmail.com>
2024-07-09 18:19:32 -0400

    User Controller + Routes updated

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,6 +1,18 @@
 const User = require("../models/UserModel");
 
 const UserController = {
+  async getAllUsers(req, res, next) {
+    try {
+      const allUsers = await User.find({});
+      res.locals.allUsers = allUsers;
+      return next();
+    } catch (err) {
+      return next({
+        log: "Express error handler caught middleware error when getting all user",
+        message: { err: "No users exist" },
+      });
+    }
+  },
   async getUser(req, res, next) {
     const userEmail = req.params.email;
     const user = await User.findOne({ email: userEmail.slice(1) });
@@ -35,6 +47,49 @@ const UserController = {
       });
     }
   },
+  async deleteUser(req, res, next) {
+    try {
+      const email = req.params.email;
+
+      // Attempt to find and delete the user
+      //
+      const user = await User.findOneAndDelete({ email });
+      console.log(user);
+      // If no user found with the specified username, return an error
+      if (user === null) {
+        return next({
+          log: "Express error handler caught middleware error when deleting user",
+          message: { err: "User does not exist" },
+        });
+      }
+
+      // Log successful deletion
+      console.log(`User deleted: ${email}`);
+      // Continue to the next middleware
+      return next();
+    } catch (err) {
+      // Handle any unexpected errors
+      return next(err); // Pass the error to the next middleware
+    }
+  },
+
+  // async updateUser(req, res, next) {
+  //   const { username, newPassword } = req.body;
+
+  //   try {
+  //     //update the user password
+  //     const updatedUser = await User.findOneAndUpdate(
+  //       { username: username },
+  //       { password: newPassword },
+  //       { new: true, useFindAndModify: false },
+  //     );
+  //     if (!updatedUser) {
+  //       return res.status(404).send("User not found");
+  //     }
+  //   } catch (err) {
+  //     res.status(500).send('Error updating password');
+  //   }
+  // },
 };
 
 module.exports = UserController;

29f630f3f3c049498171a384d39b10921761f91b
Davis Kim <dkim0509@gmail.com>
2024-07-11 20:37:15 -0400

    jest testing added for rendering of components through sidenav

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,29 +7,27 @@ const Map = () => {
       <div className="flex-column">
         <GameNavbar />
       </div>
-      <div className="interactive-container">
-        <nav>
-          <ul>
-            <li>
-              <a href="level-one">1</a>
-            </li>
-            <li>
-              <a href="level-two">2</a>
-            </li>
-            <li>
-              <a href="level-three">3</a>
-            </li>
-            <li>
-              <a href="level-four">4</a>
-            </li>
-            <li>
-              <a href="level-five">5</a>
-            </li>
-            <li>
-              <a href="level-six">6</a>
-            </li>
-          </ul>
-        </nav>
+      <div className="interactive-container" data-testid="map">
+        <ul>
+          <li>
+            <a href="level-one">1</a>
+          </li>
+          <li>
+            <a href="level-two">2</a>
+          </li>
+          <li>
+            <a href="level-three">3</a>
+          </li>
+          <li>
+            <a href="level-four">4</a>
+          </li>
+          <li>
+            <a href="level-five">5</a>
+          </li>
+          <li>
+            <a href="level-six">6</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
